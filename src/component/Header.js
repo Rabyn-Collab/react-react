03394b08704c6510627c585b10a4b05450a2f9a6
Rabyn900 <babynshrestha64@gmail.com>
2023-03-12 09:33:09 +0545

    chnages

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,12 +2,27 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Header = () => {
+
+  const navs = [
+    {
+      label: 'About',
+      path: '/about'
+    },
+    {
+      label: 'Contact',
+      path: '/contact'
+    }
+  ];
+
+
   return (
-    <div className='flex justify-between px-9 bg-purple-700 text-white py-2 items-center'>
-      <NavLink to='/' replace className='text-xl'>Logo</NavLink>
+    <div className='flex justify-between px-9   bg-black text-white py-3 items-center sticky top-0 z-50'>
+      <NavLink to='/' replace className='text-xl'>Sample Logo</NavLink>
       <div className='space-x-5'>
-        <NavLink to='/about' >About</NavLink>
-        <NavLink to='/contact'>Contact</NavLink>
+        {navs.map((n, i) => {
+          return <NavLink className='hover:bg-white hover:p-2 hover:text-black ' to={n.path} key={i} >{n.label}</NavLink>
+        })}
+
       </div>
 
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Display from './MainPage/Display'
+import Tech from './MainPage/Tech'
 
 const Home = () => {
   return (
     <div>
-      <h1>This is Home Page</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas rem eligendi velit a dignissimos vero sed nostrum ut. Fuga magni sequi perspiciatis numquam. Itaque quasi facere natus ex deleniti vel incidunt voluptatem tempora adipisci tempore officiis corporis, atque, est facilis animi aliquid doloremque impedit delectus illum repellat aspernatur sit autem.</p>
+
+
+      <Display />
+      <Tech />
+
     </div>
   )
 }
