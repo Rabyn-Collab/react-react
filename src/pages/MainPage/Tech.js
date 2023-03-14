23638f53f537bc2f71db74fc6384edde6839e390
Rabyn900 <babynshrestha64@gmail.com>
2023-03-14 09:25:00 +0545

    changes

--- src/pages/MainPage/Tech.js
+++ src/pages/MainPage/Tech.js
@@ -6,7 +6,7 @@ const Tech = () => {
 
       <h1 className='text-center text-3xl font-bold'>Technologies I use</h1>
 
-      <div className='p-10 justify-items-center grid  grid-cols-grid4'>
+      <div className='p-10 justify-items-center grid  grid-cols-grid4 gap-10'>
         <FaReact size={150} className='hover:scale-125 hover:text-rose-400 duration-200' />
         <FaPython size={150} />
         <FaPhp size={150} />
